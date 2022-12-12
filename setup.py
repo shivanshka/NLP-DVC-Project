7591e6d0c4f15906fd9a82b4747f9c8c8f599781
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-12-12 20:09:42 +0530

    steup.py updated

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "NLP-DVC-Project"
+AUTHOR_NAME = "Shivansh Kaushal"
+AUTHOR_USER_NAME = "shivanshka"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -13,12 +14,12 @@ LIST_OF_REQUIREMENTS = []
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    author=AUTHOR_NAME,
+    description="Stackoverflow Python Tags Classification",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}.git",
+    author_email="kaushal.shivansh630@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
