103bb338bac81f6d9378f0653d2df2abce708ee6
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-12-12 19:17:24 +0530

    stage 1 updated

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -21,7 +21,19 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    
+    source_data_dir = config["source_data"]["data_dir"]
+    source_data_file = config["source_data"]["data_file"]
+    source_data_path = os.path.join(source_data_dir, source_data_file)
+
+    split = params["prepare"]["split"]
+    seed = params["prepare"]["seeds"]
+
+    random.seed(seed)
+
+    artifacts = config["artifacts"]
+    prepare_data_dir_path = os.apth.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED"])
+    create_directories(prepare_data_dir_path)
 
 
 if __name__ == '__main__':
